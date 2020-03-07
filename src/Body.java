2ca3b92658eaf4953ec51c3c4ef7b8694d7cf20c
jozott <e11911133@student.tuwien.ac.at>
2020-03-07 23:37:27 +0100

    Abgabe UE1 Johannes Zottele v1.0

--- src/Body.java
+++ src/Body.java
@@ -5,5 +5,98 @@
 // gravitational force of another mass).
 public class Body {
     //TODO: class definition.
+    /*
+    This class should describe the object that will be created later in "space.java".
+    For this purpose some variables and methods are needed.
+    --- Variables:
+        - Current 3-dimensional position of the object (posX, posY, posZ)
+        - Current velocity of the object as velocity vector (velX, velY, velZ)
+        - Current mass of object (mass)
+
+    --- Methods:
+        - Body(): Constructor which is probably not needed
+        - setPosition(x, y, z): A setter to overwrite or create the current position.
+        - setVelocity(x, y, z): A setter to overwrite or create the current velocity vector.
+        - setMass(m): A setter to overwrite or create the current mass.
+        - getMass(m): A getter to return the current mass of the object.
+
+        - move(): Executes a movement of the object (simulates one second).
+                  Therefore the current position is replaced by the old position plus the current speed.
+
+        - move(fx, fy, fz): Changes the current velocity of the object by a force passed through fx, fy and fz.
+                            Current velocity vector is replaced by velocity vector plus force divided by mass.
+                            Then move() is executed to perform movement step.
+
+     Question:
+     move(seconds, fx, fy, fz) {
+        double[] pos = move(fx, fy, fz);
+        if (seconds == 1) return pos;
+        return move(seconds - 1, fx, fy, fz);
+     }
+     When the method is called, move(fx, fy, fz) is executed and the returned values are stored as pos.
+     Then it is checked if the seconds have expired ( == 1 because move() happens before seconds decrease),
+     if so the most recent position is returned. If not the method itself is called again, with one second less.
+     If the force action was one-time, you have to set the force parameter to 0 in the recursion call.
+     */
+
+    //position of obj
+    private double posX;
+    private double posY;
+    private double posZ;
+
+    //velocity of obj
+    private double velX;
+    private double velY;
+    private double velZ;
+
+    //mass of obj
+    private double mass;
+
+    public Body() {
+    }
+
+    public void setPosition(double x, double y,  double z) {
+        posX = x;
+        posY = y;
+        posZ = z;
+    }
+
+    public void setVelocity(double x, double y, double z) {
+        velX = x;
+        velY = y;
+        velZ = z;
+    }
+
+    public void setMass(double m) {
+        mass = m;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public double [] move() {
+        posX += velX;
+        posY += velY;
+        posZ += velZ;
+
+        return new double[] {posX, posY, posZ};
+    }
+
+    // new velocity:
+    // actual velocity + force / mass
+    public double[] move(double fx, double fy, double fz) {
+
+        velX += (fx / mass);
+        velY += (fy / mass);
+        velZ += (fz / mass);
+
+        System.out.println("ZForce: " + fz );
+        System.out.println("Velocity: " + velZ);
+
+        return move();
+    }
+
+
 
 }
--- src/Space.java
+++ src/Space.java
@@ -1,4 +1,8 @@
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Map;
+
 // Space is the actual program (executable class) using objects of class 'Body'.
 public class Space {
 
@@ -15,6 +19,10 @@ public class Space {
     // This means each second its speed increases about 9.82 meters per second.
 
     //TODO: further variables, if needed.
+    public static final double ROCKET_FORCE = 5e6;
+    public static final double ROCKET_START_MASS = 2.9e5;
+    public static final double ROCKET_FUEL_TANK = 2.55e5;
+    public static final double ROCKET_FUELBURN_PER_SECOND = 1333;
 
     // The main simulation method using instances of other classes.
     public static void main(String[] args) {
@@ -25,38 +33,145 @@ public class Space {
         //Results for the falling ball on the surface of earth are as follows:
         //Height 10m: 2 (sec = number of move(fx,fy,fz) calls)
         //Height 100m: 5 (sec = number of move(fx,fy,fz) calls)
-        /*
+
         Body ball1 = new Body();
         ball1.setPosition(0,0,100); // 100m height.
         ball1.setVelocity(0,0,0);
         ball1.setMass(1); // 1 kg
-        System.out.println(fallToGround(ball1)); // 5
+//        System.out.println(fallToGround(ball1)); // 5
+
 
         ball1.setPosition(0,0,10); // 10m height.
         ball1.setVelocity(0,0,0);
-        System.out.println(fallToGround(ball1)); // 2
+//        System.out.println(fallToGround(ball1)); // 2
+
 
         Body ball2 = new Body();
         ball2.setPosition(0,0,100); // 100m height.
         ball2.setVelocity(0,0,0);
         ball2.setMass(15); // 15 kg
-        System.out.println(fallToGround(ball1)); // 5
-        */
+//        System.out.println(fallToGround(ball2)); // 5
+
 
         //Further examples are to be tested (body in empty space, rocket, feather).
+
+        Body obj = new Body();
+        obj.setPosition(0,0,0);
+        obj.setVelocity(1,0.2, 0.5);
+//        System.out.println("Position after ... seconds: " + Arrays.toString(positionInEmptySpace(obj, 5)));
+
+
+        Body rocket = new Body();
+        rocket.setPosition(0,0,0);
+        rocket.setVelocity(0,0, 0);
+        rocket.setMass(ROCKET_START_MASS);
+//        System.out.println("Rocket landed after " + rocketFlightInSeconds(rocket) + " seconds");
+
+        rocket.setVelocity(0,0,0);
+        rocket.setPosition(0,0,0);
+        rocket.setMass(ROCKET_START_MASS);
+//        System.out.println("Position after ... seconds: " + Arrays.toString(positionOfRocket(rocket, 5)));
+
+
+        Body feather = new Body();
+        feather.setPosition(0,0,5);
+        feather.setVelocity(0,0,0);
+        feather.setMass(8.2e-6);
+//        System.out.println("Position after ... seconds or touching ground: " + Arrays.toString(randomWindinputOnFeather(feather, 6)));
+
 
     }
 
     // Returns the number of move(fx,fy,fz) calls needed for 'b' hitting the ground, i.e.,
     // the method reduces the z-coordinate of 'b' until it becomes 0 or negative.
     public static int fallToGround(Body b) {
-
         //TODO: implement recursive method.
-        return 0;
+        double[] pos = b.move(0.0, 0.0, -gravCalc(b.getMass()));
 
+        if (pos[2] <= 0) {
+            return 1;
+        }
+        int calls = fallToGround(b);
+        return ++calls;
     }
 
     //TODO: Define further methods as needed.
+
+    //returns position after ... seconds in empty space
+    public static double[] positionInEmptySpace(Body obj, int sec) {
+        double[] objPos = obj.move();
+
+        //if sec == 1 because move() runs before sec decrement
+        if(sec == 1)
+            return objPos;
+
+        return positionInEmptySpace(obj, sec - 1);
+    }
+
+    //returns position of rocket after ... seconds in empty space
+    public static double[] positionOfRocket(Body rocket, int sec) {
+        double[] rocketPos = rocket.move(0, 0, ROCKET_FORCE);
+
+        //check if fuel > 0, if so decrement the mass by ROCKET_FUELBURN_PER_SECOND
+        double rocketMass = rocket.getMass();
+        double fuel = rocketMass - (ROCKET_START_MASS - ROCKET_FUEL_TANK);
+        if(fuel > 0) rocket.setMass(rocketMass - ROCKET_FUELBURN_PER_SECOND);
+
+        if(sec == 1) return rocketPos;
+
+        return positionOfRocket(rocket, sec - 1);
+    }
+
+    //returns flight time in seconds after returning to ground (same as fallToGround() just with some extras)
+    public static int rocketFlightInSeconds(Body rocket) {
+        double rocketMass = rocket.getMass(); //complete rocket mass
+        double grav = gravCalc(rocketMass); //gravitational force on rocket
+        double fuel = rocketMass - (ROCKET_START_MASS - ROCKET_FUEL_TANK); //available fuel in rocket
+        double thrust = fuel > 0 ? ROCKET_FORCE : 0; //thrust of rocket (constant, only if fuel > 0)
+
+        System.out.println("Fuel: " + fuel);
+        System.out.println("Thrust: " + thrust);
+        System.out.println("GravForce: " + grav);
+
+        //calc next position of rocket (thrust force of rocket minus gravitational force on rocket) ... only z-axis
+        double[] pos = rocket.move(0.0, 0.0, thrust - gravCalc(rocketMass));
+
+        System.out.println("Position: " + Arrays.toString(pos));
+
+        // on reaching ground
+        if(pos[2] <= 0)
+            return 1;
+
+        // fuel-burn per second: 133 liter
+        if(fuel > 0)
+            rocket.setMass(rocketMass-ROCKET_FUELBURN_PER_SECOND);
+
+        System.out.println("-------------");
+
+        int calls = rocketFlightInSeconds(rocket);
+        return ++calls;
+    }
+
+    //returns position of feather after ... seconds. Position of feather is random, caused of the random wind force
+    public static double[] randomWindinputOnFeather(Body feather, int sec) {
+        double grav = gravCalc(feather.getMass());
+
+        //generate values -1 to 1. e.g: 0.4 * 1^3
+        double fx = Math.random() * Math.pow(-1, (int) (Math.random() * 10));
+        double fy = Math.random() * Math.pow(-1, (int) (Math.random() * 10));
+        double fz = Math.random() * Math.pow(-1, (int) (Math.random() * 10));
+
+        double[] featherPos = feather.move(fx, fy, fz - grav);
+
+        if(featherPos[2] <= 0 || sec == 1) return featherPos;
+
+        return randomWindinputOnFeather(feather, sec-1);
+    }
+
+    //own gravitational force calculation with variable mass
+    public static double gravCalc(double objMass) {
+        return G*MASS_OF_EARTH*objMass/(RADIUS_OF_EARTH*RADIUS_OF_EARTH);
+    }
 
 }
 
